f5b8dd000dd08506cb1dba8b3fa81c840654bc5c
SumaiyaMoon <sumaiyaaltafmoon@gmail.com>
2024-03-10 19:37:16 +0500

    Repo Updated

---
+++ screens/extra.tsx
@@ -0,0 +1,10 @@
+import React, {useState, useEffect} from 'react';
+import {Text} from 'react-native';
+
+export default function Extra() {
+  return (
+    <>
+      <Text>Dummy .tsx file to test the repo</Text>
+    </>
+  );
+}
